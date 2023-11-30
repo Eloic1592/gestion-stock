de2926220af484ee8ec6bdb46451b10f87da1381
Eloic <99272320+Eloic1592@users.noreply.github.com>
2023-11-30 15:33:35 +0200

    [BDD] UPDATE Data.sql

--- Data.sql
+++ Data.sql
@@ -4,3 +4,6 @@
 -- marque 
 -- INSERT INTO marque(marque) values('TOSHIBA'),('FUJITSU'),('LENOVO'),
 -- ('DELL'),('HP'),('NEC'),('DYNABOOK'),('SONY');
+
+-- type materiel
+-- INSERT INTO typeMateriel(typeMateriel) values('Materiel bureatique'),('Materiel Informatique'),('Autre');
