56e01f2248a20d5e0a7de8eb362b0d1e9e4761e5
Eloic <99272320+Eloic1592@users.noreply.github.com>
2023-11-30 15:25:45 +0200

    [BDD] Add data.sql

---
+++ Data.sql
@@ -0,0 +1,6 @@
+-- Couleur 
+-- INSERT INTO couleur(couleur) values('NOIR'),('GRIS');
+
+-- marque 
+-- INSERT INTO marque(marque) values('TOSHIBA'),('FUJITSU'),('LENOVO'),
+-- ('DELL'),('HP'),('NEC'),('DYNABOOK'),('SONY');
