7958e807e4a4b7f846b46a1e46c06da0ac01774d
Eloic <99272320+Eloic1592@users.noreply.github.com>
2023-12-08 10:21:06 +0200

    Add data.sql
    Add data.sql

--- Data.sql
+++ Data.sql
@@ -1,1 +1,10 @@
+-- Categorie de materiel
+INSERT INTO categorieMateriel(id,categorieMateriel) values(getseqcategoriemateriel,'Materiel bureautique');
+INSERT INTO categorieMateriel(id,categorieMateriel) values(getseqcategoriemateriel,'Materiel Informatique');
+INSERT INTO categorieMateriel(id,categorieMateriel) values(getseqcategoriemateriel,'Alimentation');
 
+-- Type de materiel
+INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'Ordinateur');
+INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'Imprimante');
+INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'chargeur');
+INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'clavier');
