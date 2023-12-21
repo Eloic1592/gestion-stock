3e4649d0246ccfd09a4adc3d706c9bef456e0204
Eloic Rakoto-Arisoa <99272320+Eloic1592@users.noreply.github.com>
2023-12-21 09:35:27 +0300

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -1,10 +1,18 @@
 -- Categorie de materiel
 INSERT INTO categorieMateriel(id,categorieMateriel) values(getseqcategoriemateriel,'Materiel bureautique');
 INSERT INTO categorieMateriel(id,categorieMateriel) values(getseqcategoriemateriel,'Materiel Informatique');
+INSERT INTO categorieMateriel(id,categorieMateriel) values(getseqcategoriemateriel,'Materiel Sonore');
 INSERT INTO categorieMateriel(id,categorieMateriel) values(getseqcategoriemateriel,'Alimentation');
+INSERT INTO categorieMateriel(id,categorieMateriel) values(getseqcategoriemateriel,'Luminaires');
+INSERT INTO categorieMateriel(id,categorieMateriel) values(getseqcategoriemateriel,'Aeration');
+
+
 
 -- Type de materiel
 INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'Ordinateur');
 INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'Imprimante');
 INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'chargeur');
 INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'clavier');
+INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'Ventilateur');
+INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'Baffle');
+INSERT INTO typeMateriel(id,typeMateriel) values(getseqtypemateriel,'Prise');
