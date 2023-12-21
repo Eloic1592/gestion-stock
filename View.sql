8ea60df25dda53f2602fbb6fcd2ddad0a951071d
Eloic Rakoto-Arisoa <99272320+Eloic1592@users.noreply.github.com>
2023-12-21 14:28:06 +0300

    Update View.sql

--- View.sql
+++ View.sql
@@ -99,3 +99,61 @@ from PAIEMENT p
          join MODEPAIEMENT mp on p.IDMODEPAIEMENT = mp.ID;
 
 select * from paiement_facture;
+
+
+-- Commande
+Drop view detail_commande;
+
+Create or replace view detail_commande as
+select dc.IDDETAILSCOMMANDE,
+       c.DATECOMMANDE,
+       a.MARQUE,
+       a.MODELE,
+       dc.IDCOMMANDE,
+       dc.DESCRIPTION,
+       dc.QUANTITE,
+       dc.PU,
+       dc.TOTAL,
+       dc.STATUT
+
+from DETAILSCOMMANDE dc
+         join ARTICLE a on dc.IDARTICLE = a.IDARTICLE
+         join COMMANDE c on dc.IDCOMMANDE = c.IDCOMMANDE;
+
+-- Devis
+
+Drop view detail_devis;
+
+Create or replace view detail_devis as
+select dv.IDDETAILDEVIS,
+       c.IDDEVIS,
+       a.MARQUE,
+       a.MODELE,
+       dv.DESCRIPTION,
+       dv.QUANTITE,
+       dv.PU,
+       dv.TOTAL
+
+from DETAILDEVIS dv
+         join ARTICLE a on dv.IDARTICLE = a.IDARTICLE
+         join DEVIS c on dv.IDDEVIS = c.IDDEVIS;
+
+-- Detail bon de commande
+drop view v_detail_bon;
+
+CREATE OR REPLACE VIEW v_detail_bon AS
+SELECT
+    db.IDDETAILBONCOMMANDE,
+    db.IDBONCOMMANDE,
+    BC.DATEBONCOMMANDE,
+    a.MARQUE,
+    a.MODELE,
+    db.DESCRIPTION,
+    db.QUANTITE,
+    db.PU,
+    db.TOTAL
+FROM DETAILBONCOMMANDE db
+         JOIN ARTICLE a ON a.IDARTICLE = a.IDARTICLE
+         JOIN DETAIL_FACTURE DF ON a.IDARTICLE = DF.IDARTICLE
+         JOIN BONCOMMANDE BC ON bc.IDBONCOMMANDE = db.IDBONCOMMANDE;
+
