c01ba2c87fd8e8cffc79c0fba6f06f4305f813a2
Eloic Rakoto-Arisoa <99272320+Eloic1592@users.noreply.github.com>
2024-02-21 10:24:47 +0300

    commit

--- Stock(clean).sql
+++ Stock(clean).sql
@@ -17,11 +17,6 @@ CREATE SEQUENCE SEQFACTUREMATERIEL START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE SEQDETAILSFACTUREMATERIEL START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE SEQPAIEMENT START WITH 1 INCREMENT BY 1;
 
--- Ordinateur,chargeur,clavier,lampe,...
-CREATE TABLE typeMateriel(
-    idtypeMateriel VARCHAR2(100) PRIMARY KEY NOT NULL ,
-    typeMateriel VARCHAR2(100) NOT NULL 
-);
 
 -- Type materiel bureautique ou materiel informatique ou autre
 CREATE TABLE categorieMateriel(
@@ -29,14 +24,38 @@ CREATE TABLE categorieMateriel(
     categorieMateriel VARCHAR2(100) NOT NULL 
 );
 
+-- Ordinateur,chargeur,clavier,lampe,...
+CREATE TABLE typeMateriel(
+    idtypeMateriel VARCHAR2(100) PRIMARY KEY NOT NULL ,
+    typeMateriel VARCHAR2(100) NOT NULL,
+    idcategorieMateriel VARCHAR2(100) NOT NULL
+);
+
+ALTER TABLE typeMateriel ADD FOREIGN KEY (idcategorieMateriel) REFERENCES categorieMateriel(idcategorieMateriel);
+
 
 CREATE TABLE article(
     idarticle VARCHAR2(50) PRIMARY KEY NOT NULL ,
-    marque VARCHAR2(100) NOT NULL ,
-    modele NVARCHAR2(1000) NOT NULL ,
+    marque NVARCHAR2(100),
+    modele NVARCHAR2(1000),
     description NVARCHAR2(1000),
-    codearticle VARCHAR2(70) NOT NULL
+    idtypeMateriel VARCHAR2(100) NOT NULL
 );
+
+ALTER TABLE article ADD FOREIGN KEY (idtypeMateriel) REFERENCES typeMateriel(idtypeMateriel);
+
+CREATE TABLE materiel(
+    idmateriel VARCHAR2(50) PRIMARY KEY NOT NULL ,
+    marque NVARCHAR2(100),
+    modele NVARCHAR2(1000),
+    numSerie VARCHAR2(100) NOT NULL ,
+    description NVARCHAR2(1000),
+    prixVente number(10,2) NOT NULL ,
+    caution number(10,2) NOT NULL ,
+    couleur VARCHAR2(100) NOT NULL ,
+    statut number DEFAULT 0
+);
+
 
 CREATE TABLE devis(
     iddevis VARCHAR2(50) PRIMARY KEY NOT NULL ,
@@ -89,25 +108,6 @@ CREATE TABLE bonlivraison(
 
 ALTER TABLE bonlivraison ADD FOREIGN KEY(idboncommande) REFERENCES boncommande(idboncommande);
 
-
-CREATE TABLE materiel(
-    idmateriel VARCHAR2(50) PRIMARY KEY NOT NULL ,
-    idtypeMateriel VARCHAR2(100) NOT NULL ,
-    idcategorieMateriel VARCHAR2(100) NOT NULL ,
-    idarticle VARCHAR2(100) NOT NULL ,
-    numSerie VARCHAR2(100) NOT NULL ,
-    description NVARCHAR2(1000),
-    prixVente number(10,2) NOT NULL ,
-    caution number(10,2) NOT NULL ,
-    couleur VARCHAR2(100) NOT NULL ,
-    statut number DEFAULT  0
-);
-
-ALTER TABLE materiel ADD FOREIGN KEY(idtypeMateriel) REFERENCES typeMateriel(idtypeMateriel);
-ALTER TABLE materiel ADD FOREIGN KEY(idcategorieMateriel) REFERENCES categorieMateriel(idcategorieMateriel);
-ALTER TABLE materiel ADD FOREIGN KEY(idarticle) REFERENCES article(idarticle);
-
-
 CREATE TABLE depot(
     iddepot VARCHAR2(50) PRIMARY KEY NOT NULL , 
     depot varchar(100) NOT NULL 
@@ -133,18 +133,21 @@ ALTER TABLE mouvementStock ADD FOREIGN KEY(idnatureMouvement) REFERENCES natureM
 
 -- ENTREE SORTIE PHYSIQUE
 CREATE TABLE detailmouvementphysique(
-   iddetailmouvementphysique VARCHAR2(50) PRIMARY KEY NOT NULL ,
-   idmouvement VARCHAR2(50) NOT NULL ,
-   idarticle VARCHAR2(50) NOT NULL ,
-   quantite NUMBER(10,2) NOT NULL ,
-   PU number(10,2) NOT NULL ,
-   prixStock NUMBER(10,2) NOT NULL ,
-   total NUMBER(10,2) NOT NULL ,
-   iddepot VARCHAR2(50) NOT NULL ,
+   iddetailmouvementphysique VARCHAR2(50) PRIMARY KEY NOT NULL,
+   idmouvement VARCHAR2(50) NOT NULL,
+   idarticle VARCHAR2(50) NOT NULL,
+   quantite NUMBER(10,2)  DEFAULT 0,
+   PU NUMBER(10,2)  DEFAULT 0,
+   prixStock NUMBER(10,2) DEFAULT 0,
+   total NUMBER(10,2)  DEFAULT 0,
+   restestock NUMBER(10,2) DEFAULT 0,
+   iddepot VARCHAR2(50) NOT NULL,
    description NVARCHAR2(1000),
    commentaire NVARCHAR2(1000),
-   statut number DEFAULT  0
+   statut NUMBER DEFAULT 0
 );
+
+
 
 ALTER TABLE detailmouvementphysique ADD FOREIGN KEY(idmouvement) REFERENCES mouvementStock(idmouvementStock);
 ALTER TABLE detailmouvementphysique ADD FOREIGN KEY(idarticle) REFERENCES article(idarticle);
@@ -158,9 +161,9 @@ CREATE TABLE detailmouvementfictif(
     dateDeb TIMESTAMP DEFAULT  current_timestamp,
     dateFin TIMESTAMP DEFAULT  NULL,
     idetudiant varchar(50) NOT NULL ,
-    caution NUMBER(10,2),
+    caution NUMBER(10,2) ,
     idmateriel varchar2(50) NOT NULL ,
-    iddepot varchar(50) NOT NULL ,
+    iddepot varchar(50) NOT NULL,
     description NVARCHAR2(1000),
     commentaire NVARCHAR2(1000),
     statut number DEFAULT  0
@@ -195,3 +198,22 @@ CREATE TABLE paiement(
 ALTER TABLE paiement ADD FOREIGN KEY(idclient) REFERENCES client(idclient);
 ALTER TABLE paiement ADD FOREIGN KEY(idfacturemateriel) REFERENCES facturemateriel(idfacturemateriel);
 ALTER TABLE paiement ADD FOREIGN KEY(idmodepaiement) REFERENCES modepaiement(id);
+
+
+-- Drop table
+DROP TABLE paiement;
+DROP TABLE facturemateriel;
+DROP TABLE detailmouvementfictif;
+DROP TABLE detailmouvementphysique;
+DROP TABLE mouvementStock;
+DROP TABLE natureMouvement;
+DROP TABLE depot;
+DROP TABLE materiel;
+DROP TABLE bonlivraison;
+DROP TABLE boncommande;
+DROP TABLE proforma;
+DROP TABLE detaildevis;
+DROP TABLE devis;
+DROP TABLE article;
+DROP TABLE typeMateriel;
+DROP TABLE categorieMateriel;
