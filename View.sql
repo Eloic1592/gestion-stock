dbe52e4adfa756bfcbcadfb44ce5ddf869dd5bfb
Eloic Rakoto-Arisoa <99272320+Eloic1592@users.noreply.github.com>
2024-03-04 10:45:36 +0300

    Update View.sql

--- View.sql
+++ View.sql
@@ -306,9 +306,6 @@ CREATE OR REPLACE VIEW stock_article as
 select coalesce(sum(quantite),0) as quantite,la.idarticle,la.marque,la.modele,la.description,la.IDTYPEMATERIEL,la.TYPEMATERIEL
 from detailmouvementphysique dm  right join liste_article la on la.idarticle=dm.idarticle  group by la.idarticle,la.marque,la.modele,la.description,la.IDTYPEMATERIEL,la.TYPEMATERIEL;
 
--- -- Stock par materiel
--- CREATE OR REPLACE VIEW stock_materiel as
--- select tp.IDTYPEMATERIEL,tp.TYPEMATERIEL,count(lm.IDTYPEMATERIEL) as quantite from liste_materiel lm right join typemateriel tp on lm.IDTYPEMATERIEL=tp.IDTYPEMATERIEL group by tp.IDTYPEMATERIEL,tp.TYPEMATERIEL;
 
 
 -- Total des articles par depot
@@ -444,7 +441,7 @@ ORDER BY
     TousLesJours.jour;
 
 
-
+-- Liste nature mouvement
 Create or replace view liste_nature as 
 select IDNATUREMOUVEMENT,
     case TYPEMOUVEMENT WHEN 1 THEN 'PHYSIQUE' WHEN 0 THEN 'FICTIF' END as TYPEMOUVEMENT,
@@ -452,6 +449,7 @@ select IDNATUREMOUVEMENT,
     from NATUREMOUVEMENT;
 
 
+-- Stock par materiel
 CREATE OR REPLACE VIEW stock_materiel as 
 select (select count(idtypemateriel) from materiel where statut=1 AND idtypemateriel=t.idtypemateriel) as libre,(select count(idtypemateriel) from materiel where statut=0 AND idtypemateriel=t.idtypemateriel) as occupe,t.idtypemateriel,t.typemateriel from liste_materiel lm right join typemateriel t on lm.idtypemateriel=t.idtypemateriel group by t.idtypemateriel,t.typemateriel;
 
